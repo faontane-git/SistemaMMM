56bd90eee541da47f1af71a8e6d1095807bd2562
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2024-09-09 16:25:03 -0500

    Cambios

--- sistema-control/src/components/PublishPage/PublishPage.tsx
+++ sistema-control/src/components/PublishPage/PublishPage.tsx
@@ -6,20 +6,28 @@ import './PublishPage.css'; // Asegúrate de tener los estilos correctos
 const PublishPage: React.FC = () => {
   return (
     <div>
-
       <Navbar />
 
-      {/* Contenido de la página de publicación */}
       <div className="publish-container">
-        <h1 className="publish-title">Pantalla de Publicación</h1>
+        <h1 className="publish-title">Publicaciones</h1>
 
         <div className="menu">
           <ul className="menu-list">
             <li className="menu-item">
-              <Link to="/crear-publicacion" className="menu-link">Crear una nueva publicación</Link>
+              <Link to="/crear-publicacion" className="menu-link">
+                <div className="menu-card">
+                  <h2>Crear una nueva publicación</h2>
+                  <p>Comparte tus ideas o anuncios con la comunidad. Publica contenido relevante y mantente conectado.</p>
+                </div>
+              </Link>
             </li>
             <li className="menu-item">
-              <Link to="/administrar-publicaciones" className="menu-link">Administrar publicaciones</Link>
+              <Link to="/administrar-publicaciones" className="menu-link">
+                <div className="menu-card">
+                  <h2>Administrar publicaciones</h2>
+                  <p>Gestiona tus publicaciones anteriores. Edita o elimina publicaciones según sea necesario.</p>
+                </div>
+              </Link>
             </li>
           </ul>
         </div>
